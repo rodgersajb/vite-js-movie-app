a233ef052ba2bf9f9a4f86b5075489d92fa1e7a0
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-03-02 10:30:41 -0500

    imported and set up firebase

---
+++ src/Components/Firebase.jsx
@@ -0,0 +1,20 @@
+// Import initializeApp and getDatabase functions
+
+import { initializeApp} from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = firebase(getDatabase);
+
+export { firebase, db };
