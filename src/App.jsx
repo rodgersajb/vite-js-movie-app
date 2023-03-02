c4aa1ae3b6f5c2de9077a66fbf8824256a931c10
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-03-02 13:19:07 -0500

    connected to firebase, added user input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,83 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useEffect } from "react";
 import "../styles/sass/style.scss";
+import { db } from "./Components/Firebase";
+import { ref, onValue, push, set, update } from "firebase/database";
 
 function App() {
-  return <h1>HEYYYYYYYYYYYYYYY</h1>;
+  // lists, setLists in useState as an empty array
+  // userInput, setUserInput in useState as quotations
+  const [lists, setLists] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  // UseEffect to create a snapshot of firebase
+  // Convert Object into an array, mapping through the data received from the snapshot
+  // return and id of key and a copy of data with and index of key
+
+  useEffect(() => {
+    const listsRef = ref(db, "lists");
+    onValue(
+      listsRef,
+      (snapshot) => {
+        const data = snapshot.val();
+
+        const list = data
+          ? Object.keys(data).map((key) => {
+              return { id: key, ...data[key] };
+            })
+          : [];
+        setLists(list);
+      },
+      []
+    );
+  }, []);
+
+  // handleInputChange method to keep track or user input
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+  // handleFormSubmit to prevent page reloading on submit
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+  // handleSubmit method to submit user information
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const listsRef = ref(db, `lists/${userInput}`);
+    push(listsRef, userInput);
+    setUserInput("");
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>HEYYYYYYYYYYYYY</h1>
+        <form action="" onSubmit={handleFormSubmit}>
+          <label htmlFor="new-list">
+            <input
+              type="text"
+              id="new-list"
+              onChange={handleInputChange}
+              value={userInput}
+            />
+          </label>
+          <button onClick={handleSubmit}>SUBMIT</button>
+        </form>
+
+        <ul>
+          {lists.map((list) => {
+            console.log(list);
+            return (
+              <li key={list.id}>
+                <p>{list.id}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/Components/Firebase.jsx
+++ src/Components/Firebase.jsx
@@ -1,20 +1,20 @@
 // Import initializeApp and getDatabase functions
 
-import { initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: "https://vitejs-movie-pick-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-const db = firebase(getDatabase);
+const db = getDatabase(firebase);
 
 export { firebase, db };
