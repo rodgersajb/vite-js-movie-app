8b528b5c38509ff100deb51deb8598aff230f1af
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-03-17 15:25:25 -0400

    added functionality for when a user does not have a list created, will be instructed to create a list in the nav form to get started

--- src/Components/UserLists.jsx
+++ src/Components/UserLists.jsx
@@ -7,7 +7,7 @@ import MovieListCard from "./MovieListCard";
 
 const UserLists = (props) => {
   const { lists, genreOptions, userInput } = props;
- 
+
   const handleRemove = (listId) => {
     console.log(listId);
     const listRef = ref(db, `lists/${listId}`);
@@ -16,34 +16,47 @@ const UserLists = (props) => {
 
   return (
     <>
-      {lists && lists.length > 0 && (
-        <ul className="user-lists">
-          {lists.map((list, index) => {
-            return (
-              <>
-                <div key={index} className="list-container">
-                  <h3>{list.id}</h3>
-                  <button onClick={() => handleRemove(list.id)}>Remove</button>
-                </div>
-                <li className="" key={list.id}>
-                  {list.movies &&
-                    Object.entries(list.movies).map((movie, index) => {
-                      console.log(movie, "MOVIE FROM USERLIST");
-                      return (
-                        <>
-                          <MovieListCard
-                            movie={movie[1]}
-                            listId={list.id}
-                            movieKey={movie[0]}
-                          />
-                        </>
-                      );
-                    })}
-                </li>
-              </>
-            );
-          })}
-        </ul>
+      {lists.length < 1 ? (
+        <div className="no-lists">
+          <p>
+
+          You have no lists currently, create lists in the bar above to get
+          started!
+          </p>
+        </div>
+      ) : (
+        lists &&
+        lists.length > 0 && (
+          <ul className="user-lists">
+            {lists.map((list, index) => {
+              return (
+                <>
+                  <div key={index} className="list-container">
+                    <h3>{list.id}</h3>
+                    <button onClick={() => handleRemove(list.id)}>
+                      Remove
+                    </button>
+                  </div>
+                  <li className="" key={list.id}>
+                    {list.movies &&
+                      Object.entries(list.movies).map((movie, index) => {
+                        console.log(movie, "MOVIE FROM USERLIST");
+                        return (
+                          <>
+                            <MovieListCard
+                              movie={movie[1]}
+                              listId={list.id}
+                              movieKey={movie[0]}
+                            />
+                          </>
+                        );
+                      })}
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        )
       )}
 
       <UserChoices
